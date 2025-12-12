8c79f5499eabf740837c993b4425b884d8c8b6ac
Daniel Simonian <90415593+danielsimonian@users.noreply.github.com>
2025-12-12 08:39:22 -0300

    Install analytics from Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
